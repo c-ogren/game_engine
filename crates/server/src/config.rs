8ae676a589c616e6555423c3ac2849879190898a
Curtis E Ogren <maven.vqp@gmail.com>
2026-07-22 07:04:32 -0700

    dotenvy add

--- crates/server/src/config.rs
+++ crates/server/src/config.rs
@@ -10,14 +10,19 @@ pub struct Config {
 
 impl Config {
     pub fn from_env() -> Self {
+        dotenvy::dotenv().ok();
+
         let tcp_server_addr =
             env::var("TCP_SERVER_ADDR").unwrap_or_else(|_| "127.0.0.1:8080".to_string());
+
         let udp_server_addr =
             env::var("UDP_SERVER_ADDR").unwrap_or_else(|_| "127.0.0.1:8081".to_string());
+
         let scenery_count = env::var("SCENERY_COUNT")
             .ok()
             .and_then(|s| s.parse::<usize>().ok())
             .unwrap_or(10);
+
         let tick_rate: Duration = Duration::from_nanos(16_666_667); // 60 FPS, 60 Hz
         Self {
             tcp_server_addr,
